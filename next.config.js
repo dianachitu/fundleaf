18d62f59dcb84fc5b8423a54f7804ab5cc8a48a0
dianachitu <chitudiana@gmail.com>
2026-05-22 17:21:44 +0300

    Update and rename next.config.ts to next.config.js

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: { domains: ["fundleaf.ro"] },
+};
+
+module.exports = nextConfig;
--- next.config.ts
+++
@@ -1,5 +0,0 @@
-import type { NextConfig } from "next";
-const nextConfig: NextConfig = {
-  images: { domains: ["fundleaf.ro"] },
-};
-export default nextConfig;
